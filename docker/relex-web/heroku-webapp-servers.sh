5633e5eedd81791ce75710a09ef58d42f264acbf
David Hart <dhart@opencog.org>
2013-11-06 13:09:36 +0800

    RelEx Dockerfile: start both relex.PlainTextServer and org.linkgrammar.LGService

--- docker/relex-web/heroku-webapp-servers.sh
+++ docker/relex-web/heroku-webapp-servers.sh
@@ -3,4 +3,5 @@
 # see relation-extractor.sh for notes and options
 
 source ./relex-environment.sh
-java $VM_OPTS -classpath $CLASSPATH relex.PlainTextServer --relex --anaphora --port 7004
+nohup java $VM_OPTS -classpath $CLASSPATH org.linkgrammar.LGService -verbose 7001 en > ./out/org.linkgrammar.LGService &
+nohup java $VM_OPTS -classpath $CLASSPATH relex.PlainTextServer --relex --anaphora --port 7004 > ./out/relex.PlainTextServer &
