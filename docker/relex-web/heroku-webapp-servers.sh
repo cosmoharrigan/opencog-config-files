bfff02dc9662878d7eb046bbe0bf0182262edd66
David Hart <dhart@opencog.org>
2013-11-09 08:26:22 +0800

    RelEx Dockerfile: add Russian LinkGrammar

--- docker/relex-web/heroku-webapp-servers.sh
+++ docker/relex-web/heroku-webapp-servers.sh
@@ -2,8 +2,15 @@
 #
 # see relation-extractor.sh for notes and options
 
-cd /relex
-source ./relex-environment.sh
-nohup java $VM_OPTS -classpath $CLASSPATH org.linkgrammar.LGService -verbose 7001 en > ./out/org.linkgrammar.LGService &
-nohup java $VM_OPTS -classpath $CLASSPATH relex.Server --relex --anaphora --port 4444 > ./out/relex.Server &
-nohup java $VM_OPTS -classpath $CLASSPATH relex.PlainTextServer --relex --anaphora --port 7004 > ./out/relex.PlainTextServer &
+SCRIPT_HOME=$(dirname $0)
+
+source $SCRIPT_HOME/relex-environment.sh
+
+nohup java $VM_OPTS -classpath $CLASSPATH org.linkgrammar.LGService \
+    -verbose 7001 en &> $SCRIPT_HOME/out/org.linkgrammar.LGService-en-7001.log &
+nohup java $VM_OPTS -classpath $CLASSPATH relex.Server \
+    --relex --anaphora --port 7003 &> $SCRIPT_HOME/out/relex.Server-7003.log &
+nohup java $VM_OPTS -classpath $CLASSPATH relex.PlainTextServer \
+    --relex --anaphora --port 7004 &> $SCRIPT_HOME/out/relex.PlainTextServer-7004.log &
+nohup java $VM_OPTS -classpath $CLASSPATH org.linkgrammar.LGService \
+    -verbose 7006 ru &> $SCRIPT_HOME/out/org.linkgrammar.LGService-ru-7006.log &
